f385aca62bcd4128cb44f5a665da29b6ced2f463
Emilio Cobos Álvarez <emilio@crisal.io>
2025-08-08 18:33:02 +0000

    Bug 1977041 - Avoid unnecessarily rebuilding profiles menu. r=profiles-reviewers,niklas
    This is more efficient, and otherwise the native menubar code tries to
    rebuild the whole menu. That, combined with the (non-Firefox) issue
    described in bug 1975681, causes excessive resource usage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D260125

--- browser/base/content/browser-profiles.js
+++ browser/base/content/browser-profiles.js
@@ -114,27 +114,26 @@ var gProfiles = {
    */
   async onPopupShowing() {
     let menuPopup = document.getElementById("menu_ProfilesPopup");
-    // TODO(emilio, bug 1977041): Rebuilding this from scratch every time is
-    // rather inefficient and interacts poorly with the Linux native menubar.
-    while (menuPopup.hasChildNodes()) {
-      menuPopup.firstChild.remove();
-    }
-
     let profiles = await SelectableProfileService.getAllProfiles();
     let currentProfile = SelectableProfileService.currentProfile;
-
+    let insertionPoint = document.getElementById("menu_newProfile");
+    let existingItems = [...menuPopup.querySelectorAll(":scope > menuitem[profileid]")];
     for (let profile of profiles) {
-      let menuitem = document.createXULElement("menuitem");
+      let menuitem = existingItems.shift();
+      let isNewItem = !menuitem;
+      if (isNewItem) {
+        menuitem = document.createXULElement("menuitem");
+        menuitem.classList.add("menuitem-iconic", "menuitem-iconic-profile");
+        menuitem.setAttribute("command", "Profiles:LaunchProfile");
+      }
       let { themeBg, themeFg } = profile.theme;
       menuitem.setAttribute("profileid", profile.id);
-      menuitem.setAttribute("command", "Profiles:LaunchProfile");
       menuitem.style.setProperty("--menu-profiles-theme-bg", themeBg);
       menuitem.style.setProperty("--menu-profiles-theme-fg", themeFg);
       menuitem.style.setProperty(
         "--menuitem-icon",
         `url(${await profile.getAvatarURL(48)})`
       );
-      menuitem.classList.add("menuitem-iconic", "menuitem-iconic-profile");
 
       if (profile.id === currentProfile.id) {
         menuitem.classList.add("current");
@@ -144,31 +143,20 @@ var gProfiles = {
           JSON.stringify({ profileName: profile.name })
         );
       } else {
+        menuitem.classList.remove("current");
+        menuitem.removeAttribute("data-l10n-id");
+        menuitem.removeAttribute("data-l10n-args");
         menuitem.setAttribute("label", profile.name);
       }
 
-      menuPopup.appendChild(menuitem);
+      if (isNewItem) {
+        menuPopup.insertBefore(menuitem, insertionPoint);
+      }
     }
-
-    let newProfile = document.createXULElement("menuitem");
-    newProfile.id = "menu_newProfile";
-    newProfile.className = "menuitem-iconic";
-    newProfile.setAttribute("command", "Profiles:CreateProfile");
-    newProfile.setAttribute("data-l10n-id", "menu-profiles-new-profile");
-    menuPopup.appendChild(newProfile);
-
-    let separator = document.createXULElement("menuseparator");
-    separator.id = "profilesSeparator";
-    menuPopup.appendChild(separator);
-
-    let manageProfiles = document.createXULElement("menuitem");
-    manageProfiles.id = "menu_manageProfiles";
-    manageProfiles.setAttribute("command", "Profiles:ManageProfiles");
-    manageProfiles.setAttribute(
-      "data-l10n-id",
-      "menu-profiles-manage-profiles"
-    );
-    menuPopup.appendChild(manageProfiles);
+    // If there's any old item to remove, do so now.
+    for (let remaining of existingItems) {
+      remaining.remove();
+    }
   },
 
   manageProfiles() {
