baac8fe0ffd90dbc7c68fe6628933bc39637d564
agoloman <agoloman@mozilla.com>
2025-08-11 17:40:00 +0300

    Revert "Bug 1981851 - Default to rotating the context ID every 30 days. r=nanj" for causing bc failures @browser_quicksuggest_ping_amp.js.
    This reverts commit e65887e607476ecafd521586747b8307a7927dd9.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -3455,7 +3455,7 @@ pref("toolkit.contentRelevancy.log", false);
 
 // The number of days after which to rotate the context ID. 0 means to disable
 // rotation altogether.
-pref("browser.contextual-services.contextId.rotation-in-days", 30);
+pref("browser.contextual-services.contextId.rotation-in-days", 0);
 pref("browser.contextual-services.contextId.rust-component.enabled", true);
 
 // Pref to enable the IP protection feature
