8f38acea40184d9abbcaa71b46ec3b8e31746f2d
Emilio Cobos Álvarez <emilio@crisal.io>
2025-08-12 16:01:12 +0000

    Bug 1972694 - Better naming for BlitReason / backdrop root stacking context flag. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D259326

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -5024,7 +5024,7 @@ bool nsDisplayOpacity::CreateWebRenderCommands(
     params.flags |= wr::StackingContextFlags::WRAPS_BACKDROP_FILTER;
   }
   if (mForceIsolation) {
-    params.flags |= wr::StackingContextFlags::IS_BACKDROP_ROOT;
+    params.flags |= wr::StackingContextFlags::FORCED_ISOLATION;
   }
   StackingContextHelper sc(aSc, GetActiveScrolledRoot(), mFrame, this, aBuilder,
                            params);
@@ -6817,7 +6817,7 @@ bool nsDisplayTransform::CreateWebRenderCommands(
     params.flags |= wr::StackingContextFlags::WRAPS_BACKDROP_FILTER;
   }
   if (mForceIsolation) {
-    params.flags |= wr::StackingContextFlags::IS_BACKDROP_ROOT;
+    params.flags |= wr::StackingContextFlags::FORCED_ISOLATION;
   }
 
   wr::WrTransformInfo transform_info;
@@ -8486,7 +8486,7 @@ bool nsDisplayBackdropFilters::CreateWebRenderCommands(
   wr::StackingContextParams params;
   params.clip =
       wr::WrStackingContextClip::ClipChain(aBuilder.CurrentClipChainId());
-  params.flags = wr::StackingContextFlags::IS_BACKDROP_ROOT;
+  params.flags = wr::StackingContextFlags::FORCED_ISOLATION;
   StackingContextHelper sc(aSc, GetActiveScrolledRoot(), mFrame, this, aBuilder,
                            params);
 
