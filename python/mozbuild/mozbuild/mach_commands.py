8dc201b8eb911780562c2733789d11ba9ec1bfde
Alex Franchuk <afranchuk@mozilla.com>
2025-08-12 14:08:32 +0000

    Bug 1973820 p2 - Aggregate and print GTest failures r=jmaher
    This aggregates test information across all processes and displays a
    summary and test failures at the end of the log, which is convenient for
    developers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D254996

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -4,6 +4,7 @@
 
 import argparse
 import errno
+import functools
 import itertools
 import json
 import logging
@@ -21,6 +22,7 @@ from os import path
 from pathlib import Path
 
 import mozpack.path as mozpath
+from gtest.reports import AggregatedGTestReport
 from gtest.suites import get_gtest_suites, suite_filters
 from mach.decorators import (
     Command,
@@ -1145,116 +1147,150 @@ def gtest(
             pass_thru=True,
         )
 
-    import functools
+    report = AggregatedGTestReport()
 
-    from mozprocess import ProcessHandlerMixin
+    with report:
+        from mozprocess import ProcessHandlerMixin
 
-    processes = []
+        processes = []
 
-    def add_process(job_id, env, **kwargs):
-        def log_line(line):
-            # Prepend the job identifier to output
+        def add_process(job_id, env, **kwargs):
+            def log_line(line):
+                # Prepend the job identifier to output
+                command_context.log(
+                    logging.INFO,
+                    "GTest",
+                    {"job_id": job_id, "line": line.strip()},
+                    "[{job_id}] {line}",
+                )
+
+            report.set_output_in_env(env, job_id)
+
+            proc = ProcessHandlerMixin(
+                [app_path, "-unittest"],
+                cwd=cwd,
+                universal_newlines=True,
+                env=env,
+                processOutputLine=log_line,
+                **kwargs,
+            )
+            processes.append(proc)
+            return proc
+
+        if combine_suites:
+            # Use GTest sharding to create `jobs` processes
+            gtest_env["GTEST_TOTAL_SHARDS"] = str(jobs)
+
+            for i in range(0, jobs):
+                env = gtest_env.copy()
+                env["GTEST_SHARD_INDEX"] = str(i)
+                add_process(str(i), env).run()
+        else:
+            # Make one process per test suite
+            suites = get_gtest_suites(args, cwd, gtest_env)
+
+            from threading import Event, Lock
+
+            processes_to_run = []
+            all_processes_run = Event()
+            running_suites = set()
+            process_state_lock = Lock()
+
+            def run_next(finished_suite=None):
+                """
+                Run another test suite process.
+
+                If `finished_suite` is provided, it will be considered as finished.
+                This updates the `running_suites` set and will signal the
+                `all_processes_run` Event when there are no longer any test suites
+                to start (though some may still be running).
+
+                This may be safely called from different threads
+                (ProcessHandlerMixin callbacks occur from separate threads).
+                """
+                # The changes here must be synchronized, so acquire a lock for the
+                # duration of the function.
+                with process_state_lock:
+                    if finished_suite is not None:
+                        running_suites.remove(finished_suite)
+                    if len(processes_to_run) > 0:
+                        next_suite, proc = processes_to_run.pop()
+                        proc.run()
+                        running_suites.add(next_suite)
+                        command_context.log(
+                            logging.DEBUG,
+                            "GTest",
+                            {},
+                            f"Starting {next_suite} tests. {len(processes_to_run)} suites remain.",
+                        )
+                    else:
+                        all_processes_run.set()
+                    if len(running_suites) > 0:
+                        command_context.log(
+                            logging.INFO,
+                            "GTest",
+                            {},
+                            f"Currently running suites: {', '.join(running_suites)}",
+                        )
+
+            for filt in suite_filters(suites):
+                proc = add_process(
+                    filt.suite,
+                    filt(gtest_env),
+                    onFinish=functools.partial(run_next, filt.suite),
+                )
+                processes_to_run.append((filt.suite, proc))
+
+            # Start a number of processes according to 'jobs'. As they finish,
+            # they'll each kick off another one.
+            for _ in range(jobs):
+                run_next()
+
+            # Wait for all processes to have been started, then wait on completion.
+            all_processes_run.wait()
+
+        # Wait on processes and return a non-zero exit code if any process does so.
+        exit_code = 0
+        for process in processes:
+            status = process.wait()
+            if status:
+                exit_code = status
+
+        # Clamp error code to 255 to prevent overflowing multiple of
+        # 256 into 0
+        if exit_code > 255:
+            exit_code = 255
+
+    # Show aggregated report information and any test errors.
+    command_context.log(
+        logging.INFO,
+        "GTest",
+        {
+            "tests": report["tests"] - report["disabled"],
+            "failures": report["failures"],
+            "disabled": report["disabled"],
+            "suites": len(report["testsuites"]),
+        },
+        "Ran {tests} test(s) from {suites} test suite(s) ({disabled} disabled), with {failures} failure(s).",
+    )
+
+    for suite in report["testsuites"]:
+        if suite["failures"] == 0:
+            continue
+        for test in suite["testsuite"]:
+            if "failures" not in test:
+                continue
+            full_name = f"{suite['name']}.{test['name']}"
             command_context.log(
-                logging.INFO,
+                logging.ERROR,
                 "GTest",
-                {"job_id": job_id, "line": line.strip()},
-                "[{job_id}] {line}",
+                {
+                    "test": full_name,
+                    "failure_count": len(test["failures"]),
+                    "failures": "\n".join(e["failure"] for e in test["failures"]),
+                },
+                "{test} failed {failure_count} check(s):\n{failures}",
             )
-
-        proc = ProcessHandlerMixin(
-            [app_path, "-unittest"],
-            cwd=cwd,
-            universal_newlines=True,
-            env=env,
-            processOutputLine=log_line,
-            **kwargs,
-        )
-        processes.append(proc)
-        return proc
-
-    if combine_suites:
-        # Use GTest sharding to create `jobs` processes
-        gtest_env["GTEST_TOTAL_SHARDS"] = str(jobs)
-
-        for i in range(0, jobs):
-            env = gtest_env.copy()
-            env["GTEST_SHARD_INDEX"] = str(i)
-            add_process(str(i), env).run()
-    else:
-        # Make one process per test suite
-        suites = get_gtest_suites(args, cwd, gtest_env)
-
-        from threading import Event, Lock
-
-        processes_to_run = []
-        all_processes_run = Event()
-        running_suites = set()
-        process_state_lock = Lock()
-
-        def run_next(finished_suite=None):
-            """
-            Run another test suite process.
-
-            If `finished_suite` is provided, it will be considered as finished.
-            This updates the `running_suites` set and will signal the
-            `all_processes_run` Event when there are no longer any test suites
-            to start (though some may still be running).
-
-            This may be safely called from different threads
-            (ProcessHandlerMixin callbacks occur from separate threads).
-            """
-            # The changes here must be synchronized, so acquire a lock for the
-            # duration of the function.
-            with process_state_lock:
-                if finished_suite is not None:
-                    running_suites.remove(finished_suite)
-                if len(processes_to_run) > 0:
-                    next_suite, proc = processes_to_run.pop()
-                    proc.run()
-                    running_suites.add(next_suite)
-                    command_context.log(
-                        logging.DEBUG,
-                        "GTest",
-                        {},
-                        f"Starting {next_suite} tests. {len(processes_to_run)} suites remain.",
-                    )
-                else:
-                    all_processes_run.set()
-                if len(running_suites) > 0:
-                    command_context.log(
-                        logging.INFO,
-                        "GTest",
-                        {},
-                        f"Currently running suites: {', '.join(running_suites)}",
-                    )
-
-        for filt in suite_filters(suites):
-            proc = add_process(
-                filt.suite,
-                filt(gtest_env),
-                onFinish=functools.partial(run_next, filt.suite),
-            )
-            processes_to_run.append((filt.suite, proc))
-
-        # Start a number of processes according to 'jobs'. As they finish,
-        # they'll each kick off another one.
-        for _ in range(jobs):
-            run_next()
-
-        # Wait for all processes to have been started, then wait on completion.
-        all_processes_run.wait()
-
-    # Wait on processes and return a non-zero exit code if any process does so.
-    exit_code = 0
-    for process in processes:
-        status = process.wait()
-        if status:
-            exit_code = status
-
-    # Clamp error code to 255 to prevent overflowing multiple of
-    # 256 into 0
-    if exit_code > 255:
-        exit_code = 255
 
     return exit_code
 
