1181844ed92b17d377174ad57c00b57724efa771
Jon Coppeard <jcoppeard@mozilla.com>
2025-08-13 14:14:07 +0000

    Bug 1982328 - Part 5: Replace use of std::ignore with cast to void r=allstarschh
    Casting to void is the preferred way to ignore the result of an expression.
    
    Differential Revision: https://phabricator.services.mozilla.com/D260668

--- js/src/vm/Modules.cpp
+++ js/src/vm/Modules.cpp
@@ -1961,7 +1961,7 @@ static bool ModuleEvaluate(JSContext* cx, Handle<ModuleObject*> moduleArg,
     // uncatchable exceptions.
     Rooted<Value> error(cx);
     if (cx->isExceptionPending()) {
-      std::ignore = cx->getPendingException(&error);
+      (void)cx->getPendingException(&error);
       cx->clearPendingException();
     }
 
@@ -2318,7 +2318,7 @@ static void RejectExecutionWithPendingException(JSContext* cx,
   // undefined. Not much more we can do.
   RootedValue exception(cx);
   if (cx->isExceptionPending()) {
-    std::ignore = cx->getPendingException(&exception);
+    (void)cx->getPendingException(&exception);
   }
   cx->clearPendingException();
   AsyncModuleExecutionRejected(cx, module, exception);
@@ -2706,8 +2706,8 @@ static bool TryStartDynamicModuleImport(JSContext* cx, HandleScript script,
   //
   // The host layer is responsible for calling FinishLoadingImportedModule,
   // regardless of whether it succeeds or fails.
-  std::ignore = moduleLoadHook(cx, /* referrer */ nullptr, referencingPrivate,
-                               moduleRequest, payload);
+  (void)moduleLoadHook(cx, /* referrer */ nullptr, referencingPrivate,
+                       moduleRequest, payload);
   return true;
 }
 
