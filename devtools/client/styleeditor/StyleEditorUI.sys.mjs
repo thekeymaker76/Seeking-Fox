09352e254d0aa73cb92ec32e37750bb95fd85fca
Cristina Horotan <chorotan@mozilla.com>
2025-08-19 21:27:09 +0300

    Revert "Bug 1981937 - [devtools] Don't clear stylesheets when selecting a document in Browser Toolbox iframe picker. r=devtools-reviewers,jdescottes." for causing dt failures on browser_styleeditor_filter.js
    This reverts commit ba6669879a70408958080a0e8982d88a353fea4c.

--- devtools/client/styleeditor/StyleEditorUI.sys.mjs
+++ devtools/client/styleeditor/StyleEditorUI.sys.mjs
@@ -1658,13 +1658,7 @@ export class StyleEditorUI extends EventEmitter {
         continue;
       }
 
-      if (
-        resource.name === "will-navigate" &&
-        // When selecting a document in the Browser Toolbox iframe picker, we're getting
-        // a will-navigate event. In such case, we don't want to clear the list (see Bug 1981937)
-        (!this.targetCommand.descriptorFront.isBrowserProcessDescriptor ||
-          !resource.isFrameSwitching)
-      ) {
+      if (resource.name === "will-navigate") {
         this.#startLoadingStyleSheets();
         this.#clear();
       } else if (resource.name === "dom-complete") {
