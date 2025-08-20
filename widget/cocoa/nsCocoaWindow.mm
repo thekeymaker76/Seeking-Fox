714d856d431386dff14a2717ab4662cda0439142
Brad Werth <werth@efn.org>
2025-08-20 19:53:01 +0000

    Bug 1981793 Part 3: Narrow the lock in nsCocoaWindow::Destroy. r=mstange
    Narrowing this lock ensures that macOS in-process compositor can handle a
    nsCocoaWindow::Destroy() during a paint event. This lock was expanded
    during in Bug 1889757 while developing an earlier version of the patch,
    and is not actually necessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D261642

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -4938,13 +4938,6 @@ nsresult nsCocoaWindow::CreateNativeWindow(const NSRect& aRect,
 }
 
 void nsCocoaWindow::Destroy() {
-  // Make sure that no composition is in progress while disconnecting
-  // ourselves from the view. This has to be held through the call
-  // to nsBaseWidget::Destroy (which calls ::DestroyCompositor), and
-  // that is called at the very end. So grab the lock now and keep it
-  // for the entire scope.
-  MutexAutoLock lock(mCompositingLock);
-
   if (mOnDestroyCalled) {
     return;
   }
@@ -4961,7 +4954,12 @@ void nsCocoaWindow::Destroy() {
   // (Bug 891424)
   Show(false);
 
-  [mChildView widgetDestroyed];
+  {
+    // Make sure that no composition is in progress while disconnecting
+    // ourselves from the view.
+    MutexAutoLock lock(mCompositingLock);
+    [mChildView widgetDestroyed];
+  }
 
   TearDownView();  // Safe if called twice.
   if (mFullscreenTransitionAnimation) {
