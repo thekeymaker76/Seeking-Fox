0fc049c290c0e235d4d8b37905133f2fdecbdb31
pstanciu <pstanciu@mozilla.com>
2025-08-21 11:15:14 +0300

    Revert "Bug 1984186 - Compute proper internalLeading for vertical font metrics. r=layout-jp-market-reviewers,TYLin" for causing build bustages @gfxFont.cpp
    This reverts commit 8612abd60e3b78070a18909c51225e02f842eaf6.

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -4302,8 +4302,12 @@ void gfxFont::CalculateDerivedMetrics(Metrics& aMetrics) {
   }
 
   aMetrics.maxHeight = aMetrics.maxAscent + aMetrics.maxDescent;
-  aMetrics->internalLeading =
-      std::max(0.0, aMetrics->maxHeight - aMetrics->emHeight);
+
+  if (aMetrics.maxHeight - aMetrics.emHeight > 0.0) {
+    aMetrics.internalLeading = aMetrics.maxHeight - aMetrics.emHeight;
+  } else {
+    aMetrics.internalLeading = 0.0;
+  }
 
   aMetrics.emAscent =
       aMetrics.maxAscent * aMetrics.emHeight / aMetrics.maxHeight;
@@ -4669,12 +4673,9 @@ void gfxFont::CreateVerticalMetrics() {
 
   // Somewhat arbitrary values for now, subject to future refinement...
   metrics->spaceWidth = metrics->aveCharWidth;
+  metrics->maxHeight = metrics->maxAscent + metrics->maxDescent;
   metrics->xHeight = metrics->emHeight / 2;
   metrics->capHeight = metrics->maxAscent;
-
-  metrics->maxHeight = metrics->maxAscent + metrics->maxDescent;
-  metrics->internalLeading =
-      std::max(0.0, metrics->maxHeight - metrics->emHeight);
 
   if (metrics->zeroWidth < 0.0) {
     metrics->zeroWidth = metrics->aveCharWidth;
