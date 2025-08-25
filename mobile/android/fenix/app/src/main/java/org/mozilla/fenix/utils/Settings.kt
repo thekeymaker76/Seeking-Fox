c8e2b2dc6660ed4c76d2fd9cbb9dc8f842066e78
Mugurell <Mugurell@users.noreply.github.com>
2025-08-25 14:55:39 +0000

    Bug 1984391 - Allow the composable toolbar ride the trains r=android-reviewers,skhan
    Differential Revision: https://phabricator.services.mozilla.com/D261978

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2086,7 +2086,7 @@ class Settings(private val appContext: Context) : PreferencesHolder {
     var shouldUseComposableToolbar by lazyFeatureFlagPreference(
         key = appContext.getPreferenceKey(R.string.pref_key_enable_composable_toolbar),
         default = { FxNimbus.features.composableToolbar.value().enabled },
-        featureFlag = FeatureFlags.composableToolbar,
+        featureFlag = true,
     )
 
     /**
