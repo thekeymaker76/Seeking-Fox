a5d7b5634b073a6b9e0d9f0f7df163d10cba478b
Alex Jakobi <ajakobi@mozilla.com>
2025-08-25 07:34:31 +0000

    Bug 1984621 - Remove unused startX and startY variables from touch-simulator.js r=devtools-reviewers,nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D262114

--- devtools/server/actors/emulation/touch-simulator.js
+++ devtools/server/actors/emulation/touch-simulator.js
@@ -149,8 +149,6 @@ class TouchSimulator {
           this._contextMenuTimeout = this.sendContextMenu(evt);
         }
 
-        this.startX = evt.pageX;
-        this.startY = evt.pageY;
         this.previousScreenY = evt.screenY;
 
         type = "touchstart";
